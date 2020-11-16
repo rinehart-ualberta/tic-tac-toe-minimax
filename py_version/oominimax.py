3d9780df4b84ff03cea13b2f0f9de9469012817c
rinehart-ualberta <rinehart@ualberta.ca>
2020-11-16 15:03:52 -0700

    Create oominimax.py base
    First Version of oo version of minimax with base concept

---
+++ py_version/oominimax.py
@@ -0,0 +1,44 @@
+from math import inf as infinity
+from random import choice
+from random import seed as randomseed       # Paul Lu
+import platform
+import time
+from os import system
+
+"""
+An implementation of Minimax AI Algorithm in Tic Tac Toe,
+using Python.
+This software is available under GPL license.
+Author: Clederson Cruz
+Year: 2017
+License: GNU GENERAL PUBLIC LICENSE (GPL)
+
+Example:
+Paul Lu
+CCID:  rinehart
+ID:    1670709
+
+Weekly assignment #6
+"""
+
+class Board:
+    def __init__():
+        return
+    def __str__():
+        return
+    def __repr__():
+        return
+
+class State:
+    def __init__():
+        return
+    def __str__():
+        return
+    def __repr__():
+        return
+
+
+
+if __name__ == '__main__':
+    board = Board()
+    state = State()
